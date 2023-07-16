489b8bac016e3bf283c5af12672d4cb2175f6a60
Tom Hanks <ruku508501@gmail.com>
2023-07-16 12:42:49 -0400

    First spec

---
+++ cypress/e2e/map_query.cy.js
@@ -0,0 +1,10 @@
+
+describe("cy-map", () => {
+    it("Test mapChain", () => {
+
+        let names = ["Brad", "Smith", "Matt", "Wilmer"]
+
+        cy.wrap(names).mapChain(name => cy.request(`https://api.genderize.io/?name=${name}`)).then(result => cy.log(result.body))
+       
+    })
+})
