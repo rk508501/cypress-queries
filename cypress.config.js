e1defb9b79cb104e4b2a964afa31167005f0b98d
“Heisenberg” <ruku508501@gmail.com>
2024-10-19 09:05:02 -0400

    Cy task added for printing absolute paths to the console.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        logAbsolutePaths() {
+          const folderToSearch = path.join(config.projectRoot, 'cypress', 'integration');
+
+          const files = fs.readdirSync(folderToSearch);
+          const specFiles = files.filter(file => file.endsWith('.spec.js'));
+          const absolutePaths = specFiles.map(file => path.resolve(folderToSearch, file));
+
+          // Log to terminal using console.log
+          console.log('Absolute Paths:', absolutePaths);
+
+          // Return null to signal task completion
+          return null;
+        }
+      });
     },
   },
 });
